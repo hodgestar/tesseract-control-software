f9fad167cb1ab110e4a32dff689fd0865064a4d0
Simon Cross <hodgestar@gmail.com>
2018-11-03 13:02:49 +0200

    Light up layer by layer.

--- tessled/effects/animations/poweron.py
+++ tessled/effects/animations/poweron.py
@@ -15,7 +15,11 @@ class PowerOn(Animation):
     }
 
     def post_init(self):
+        self._layer = 0
         self._frame = self.fc.empty_frame()
 
     def render(self, frame):
+        self._frame[self._layer] = 0
+        self._layer = (self._layer + 1) % (self.fc.frame_shape[2])
+        self._frame[self._layer] = 255
         frame[:] = self._frame
