f2a599cedec5bf7f2b536cda8e6374785d91feda
Simon Cross <hodgestar@gmail.com>
2018-11-03 18:09:26 +0200

    Upgrade cube sprite.

--- tessled/effects/sprites/cube.py
+++ tessled/effects/sprites/cube.py
@@ -2,17 +2,26 @@
 
 """ Cube sprites.
 
-    A cube that can be any size divisible by 2 and anywhere.
+    A cube that can be any size and anywhere.
 """
 
 from ..engine import Sprite
 
 
 class Cube(Sprite):
+    """ Cube sprite.
 
-    def __init__(self, pos=(0, 0, 0), size=4):
+        :param tuple pos:
+            Position of the corner nearest the origin as (X, Y, Z).
+            Note: This is not the same order as for frames!
+        :param int size:
+            The size of the cube in LEDs.
+    """
+
+    def __init__(self, pos=(0, 0, 0), size=4, intensity=255):
         self.pos = pos
         self.size = size
+        self.intensity = intensity
 
     def step(self):
         pass
@@ -21,18 +30,19 @@ class Cube(Sprite):
         sx, sy, sz = self.pos  # small values
         inc = self.size - 1
         bx, by, bz = sx + inc, sy + inc, sz + inc  # big values
+        intensity = self.intensity
 
-        frame[sz, sy, sx:bx] = 255
-        frame[sz, sy:by, sx] = 255
-        frame[sz, by, sx:bx] = 255
-        frame[sz, sy:by, bx] = 255
+        frame[sz, sy, sx:bx] = intensity
+        frame[sz, sy:by, sx] = intensity
+        frame[sz, by, sx:bx] = intensity
+        frame[sz, sy:by, bx] = intensity
 
-        frame[bz, sy, sx:bx] = 255
-        frame[bz, sy:by, sx] = 255
-        frame[bz, by, sx:bx] = 255
-        frame[bz, sy:by, bx] = 255
+        frame[bz, sy, sx:bx] = intensity
+        frame[bz, sy:by, sx] = intensity
+        frame[bz, by, sx:bx] = intensity
+        frame[bz, sy:by, bx] = intensity
 
-        frame[sz:bz, sy, sx] = 255
-        frame[sz:bz, sy, bx] = 255
-        frame[sz:bz, by, sx] = 255
-        frame[sz:bz, by, bx] = 255
+        frame[sz:bz, sy, sx] = intensity
+        frame[sz:bz, sy, bx] = intensity
+        frame[sz:bz, by, sx] = intensity
+        frame[sz:bz, by, bx] = intensity
